25b85364101fd9d0b67403a419d4f3e31af955cf
Sumit Agrawal <sumit.r.agrawal@hotmail.com>
2018-03-25 13:21:49 +0530

    cleaup :eyes:

--- 02 People Picker/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  web: 'https://tenant.sharepoint.com/sites/Test/'
-};
--- hello-world/e2e/app.e2e-spec.ts
+++ hello-world/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('sp-ng App', () => {
+describe('hello-world App', () => {
   let page: AppPage;
 
   beforeEach(() => {
