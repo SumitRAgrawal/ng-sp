737d14ee2794b2b41e9f20e0ba8fdeac9d68e44f
Sumit Agrawal <sumit.r.agrawal@hotmail.com>
2018-03-25 11:53:20 +0530

    people picker snippet :eyes:

---
+++ 02 People Picker/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AutoCompleteModule } from 'primeng/autocomplete';
+
+import { AppComponent } from './app.component';
+import { PeoplePickerComponent } from './people-picker/people-picker.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { SPService } from './services/sp.service';
+
+@NgModule({
+  declarations: [AppComponent, PeoplePickerComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, AutoCompleteModule, HttpClientModule],
+  providers: [SPService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
